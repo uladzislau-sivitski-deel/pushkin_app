58aeda1bebe2caef8c3c8128752b822769bd5cbc
Sivitski Vladislav <vladosiv@gmail.com>
2015-07-15 23:08:21 +0300

    new

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -4,18 +4,20 @@ TOKEN = '34291703b59f5c7e827d31116f0bf161'.freeze
 
   def index
 
+    render nothing: true
+
   if params[:level] == 1
   @question=params[:question]
   @id = params[:id]
   results = Poem.content(@question)
-  uri = URI("http://pushkin-contest.ror.by/quiz")
+  uri = URI("http://pushkin.rubyroidlabs.com/quiz")
   parameters = {
     answer: results.first.title,
     token: TOKEN,
     task_id:  @id
   }
   Net::HTTP.post_form(uri, parameters)
-  render nothing: true
+
   end
 
   end
