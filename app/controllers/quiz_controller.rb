85526bdcd125d562ce16742d02f380f9231f1dd1
Sivitski Vladislav <vladosiv@gmail.com>
2015-07-15 19:18:52 +0300

    new

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -3,23 +3,17 @@ class QuizController < ApplicationController
 TOKEN = '34291703b59f5c7e827d31116f0bf161'.freeze
 
   def index
-  if params[:level] == 1 do
+  if params[:level] == 1
   @question=params[:question]
   @id = params[:id]
-
   results = Poem.content(@question)
-
-
   uri = URI("http://pushkin-contest.ror.by/quiz")
-
- parameters = {
-  answer: results.first.title,
-  token: TOKEN,
-  task_id:  @id
- }
-
-
-Net::HTTP.post_form(uri, parameters)
+  parameters = {
+    answer: results.first.title,
+    token: TOKEN,
+    task_id:  @id
+  }
+  Net::HTTP.post_form(uri, parameters)
   end
 
   end
