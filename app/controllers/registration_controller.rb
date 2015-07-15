77c0da1b6f1201c280482df62b98504bcca31db1
Sivitski Vladislav <vladosiv@gmail.com>
2015-07-15 13:30:43 +0300

    init

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,26 +2,26 @@ class RegistrationController < ApplicationController
 
   def index
 
-    if request.post?
-    @question = params[:question]
+  # if request.post?
+ #   @question = params[:question]
     Rails.application.config.my_config = params[:token]
 
-    str1 = @question.gsub('%WORD%','')
-    str2 = @question.split('%WORD%')[0]
-    str3 = @question.split('%WORD%')[1]
+  #  str1 = @question.gsub('%WORD%','')
+  #  str2 = @question.split('%WORD%')[0]
+   # str3 = @question.split('%WORD%')[1]
 
-    result = Poem.search do
-      fulltext str1 do
-      query_phrase_slop 1
-    end
+   # result = Poem.search do
+    #  fulltext str1 do
+   #   query_phrase_slop 1
+   # end
+ # end
+
+   # answer = result.results[0].content
+  #  answer.gsub!(/[\n]/,' ')
+   # answer = answer[answer.index(str2),answer.index(str3)]
+   # answer.gsub!(str2,' ')
+
+    render json: {answer: 'снежные'}
   end
-
-    answer = result.results[0].content
-    answer.gsub!(/[\n]/,' ')
-    answer = answer[answer.index(str2),answer.index(str3)]
-    answer.gsub!(str2,' ')
-
-    render json: {answer: answer.lstip}
-  end
-end
+#end
 end
